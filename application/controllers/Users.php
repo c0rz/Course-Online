ac5f7af01bcbb040ccec246610c0b932c4ad2625
Cornelius Alfredo <sinambelaalfredo@gmail.com>
2020-03-19 12:02:10 +0700

    update

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -15,6 +15,59 @@ Class Users Extends REST_Controller {
         $this->load->model('user');
     }
 
+    public function index()
+    {
+      // Konfigurasi email
+        $config = [
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8',
+            'protocol'  => 'smtp',
+            'smtp_host' => 'smtp.gmail.com',
+            'smtp_user' => 'sinambelaalfredo@gmail.com',  // Email gmail
+            'smtp_pass'   => 'alfredo1999',  // Password gmail
+            'smtp_crypto' => 'ssl',
+            'smtp_port'   => 465,
+            'crlf'    => "\r\n",
+            'newline' => "\r\n"
+        ];
+
+        // Load library email dan konfigurasinya
+        $this->load->library('email', $config);
+
+        // Email dan nama pengirim
+        $this->email->from('no-reply@masrud.com', 'MasRud.com');
+
+        // Email penerima
+        $this->email->to('sinambelacornelius@domain.com'); // Ganti dengan email tujuan
+
+        // Lampiran email, isi dengan url/path file
+        $this->email->attach('https://masrud.com/content/images/20181215150137-codeigniter-smtp-gmail.png');
+
+        // Subject email
+        $this->email->subject('Kirim Email dengan SMTP Gmail CodeIgniter | MasRud.com');
+
+        // Isi email
+        $this->email->message("Ini adalah contoh email yang dikirim menggunakan SMTP Gmail pada CodeIgniter.<br><br> Klik <strong><a href='https://masrud.com/post/kirim-email-dengan-smtp-gmail' target='_blank' rel='noopener'>disini</a></strong> untuk melihat tutorialnya.");
+
+        // Tampilkan pesan sukses atau error
+        if ($this->email->send()) {
+            echo 'Sukses! email berhasil dikirim.';
+        } else {
+            echo 'Error! email tidak dapat dikirim.';
+        }
+    }
+
+    public function forget_post() {
+        $email = $this->post('email');
+        $con['returnType'] = 'count';
+        $con['conditions'] = array(
+            'email' => $email,
+        );
+        $userCount = $this->user->getData($con);
+        if ($userCount) {
+
+        }
+    }
     public function Profile_get() {
         $data = $this->verify_request();
         $status = parent::HTTP_OK;
