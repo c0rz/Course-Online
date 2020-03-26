0b7105cea94163123d1202ec29f4e45ee00ee39f
Cornelius Alfredo <sinambelaalfredo@gmail.com>
2020-03-26 20:22:15 +0700

    log

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -76,11 +76,9 @@ Class Users Extends REST_Controller {
     public function profile_post() {
         $data = $this->verify();
         if ($data) {
-            $id_akun = $data['data'];
+            $id_akun = $data["data"];
             $con['returnType'] = 'single';
-            $con['conditions'] = array(
-                'id_akun' => $id_akun
-            );
+            $con['conditions'] = ['id_akun' => $id_akun];
             $user = $this->user->getData($con);
             var_dump($user);
         } else {
