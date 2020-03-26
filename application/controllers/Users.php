cf30e3e8f722e8039195c05c6d955ce8e1c498d5
Cornelius Alfredo <sinambelaalfredo@gmail.com>
2020-03-26 20:30:30 +0700

    log

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -75,7 +75,7 @@ Class Users Extends REST_Controller {
 
     public function profile_post() {
         $data = $this->verify();
-        var_dump($data["data"]);
+        var_dump($data->data);
         exit();
         if ($data) {
             $id_akun = $data["data"];
