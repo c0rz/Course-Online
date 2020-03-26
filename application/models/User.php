5be5960217284a7779a5f69803f194ec67b67036
Cornelius Alfredo <sinambelaalfredo@gmail.com>
2020-03-26 20:37:46 +0700

    log

--- application/models/User.php
+++ application/models/User.php
@@ -19,7 +19,7 @@ class User extends CI_Model
             }
         }
 		if (array_key_exists("id_akun",$params)) {
-            $this->db->where('id_akun',$params['id']);
+            $this->db->where('id_akun',$params['id_akun']);
             $query = $this->db->get();
             $result = $query->row_array();
         } else {
